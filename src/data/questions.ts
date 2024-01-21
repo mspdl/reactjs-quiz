f7006ff4b045e0e779157cdbee804b3d984b9d3c
Morgan <morgan.espindola@gmail.com>
2024-01-21 20:26:40 -0300

    Created main interface

---
+++ src/data/questions.ts
@@ -0,0 +1,14 @@
+import { Question } from "@/types/Question";
+
+export const questions: Question[] = [
+  {
+    question: "Which ingredient cannot be missing?",
+    options: ["Flour", "Dye", "Water", "Sugar"],
+    answer: 1,
+  },
+  {
+    question: "Which ingredient cannot be missing?",
+    options: ["Flour", "Dye", "Water", "Sugar"],
+    answer: 1,
+  },
+];
